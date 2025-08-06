c302eda7686bdc1b3e4a93ba721d85b4c03dea9a
Atikur Rahman <fulldevatikur@gmail.com>
2025-08-06 16:56:08 +0600

    first commit

---
+++ src/Components/Countries/Countries.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import Country from "../../Country/Country";
+
+const Countries = () => {
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+        fetch("https://restcountries.com/v3.1/independent?status=true")
+        .then(res => res.json())
+        .then(data => setCountries(data))
+    }, []);
+
+    return (
+        <div className="countries">
+            <h3>Countries: {countries.length}</h3>
+            <div className="mainBox">
+                {
+                    countries.map(country => <Country key={country.cca2} country={country}></Country>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Countries;
